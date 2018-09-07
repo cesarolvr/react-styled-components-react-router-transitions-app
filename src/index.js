c7703e482df51b7022aebbaa4668d403961a0e42
cesarolvr <cesar2012oliveira@gmail.com>
2018-09-07 17:10:05 -0300

    right rules to eslint applied, but remains kill all errors

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 
 import { THEME } from './utils/js/consts';
-import App from './App.jsx';
+import App from './App';
 import './utils/css/root.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <ThemeProvider theme={THEME}>
     <App />
-  </ThemeProvider>
-, document.getElementById('root'));
+  </ThemeProvider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
