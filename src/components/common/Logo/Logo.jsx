04cb43db806ba19932d24eb4087b9cf562f539c4
cesarolvr <cesar2012oliveira@gmail.com>
2018-09-10 16:30:12 -0300

    :sparkles: path of assets fixed

--- src/components/common/Logo/Logo.jsx
+++ src/components/common/Logo/Logo.jsx
@@ -10,12 +10,12 @@ const Logo = () => (
   <StyledLogo width="120px" height="22px" viewBox="0 0 240 22" version="1.1" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink">
     <desc>Created with Sketch.</desc>
     <defs></defs>
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" font-size="27.0115">
-        <text id="CARS" font-family="Rockwell-Regular, Rockwell" font-weight="normal" letter-spacing="-0.7445404" fill="#071629">
+    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" fontSize="27.0115">
+        <text id="CARS" fontFamily="Rockwell-Regular, Rockwell" fontWeight="normal" letterSpacing="-0.7445404" fill="#071629">
             <tspan x="-1" y="21">C</tspan>
-            <tspan x="19.7143026" y="21" letter-spacing="0.055">ARS</tspan>
+            <tspan x="19.7143026" y="21" letterSpacing="0.055">ARS</tspan>
         </text>
-        <text id="VENTURES" font-family="Rockwell-Bold, Rockwell" font-weight="bold" letter-spacing="0.055" fill="#C81120">
+        <text id="VENTURES" fontFamily="Rockwell-Bold, Rockwell" fontWeight="bold" letterSpacing="0.055" fill="#C81120">
             <tspan x="81" y="21">VENTURES</tspan>
         </text>
     </g>
